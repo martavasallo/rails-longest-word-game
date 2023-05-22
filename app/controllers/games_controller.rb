7eddd5db84a282de546ba9c142594d19714c6833
marta vasallo <mvasalloperez@gmail.com>
2023-05-22 16:01:56 +0100

    longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,11 @@
 class GamesController < ApplicationController
 
   def new
-  
+    alphabet = ('A'..'Z').to_a
+    @letters = alphabet.sample(10).join('')
+  end
+
+  def score
+    @result = params[:result]
   end
 end
